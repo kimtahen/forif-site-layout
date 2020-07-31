71426f7d24b93a1db267a4a802e04cbeedb10596
kimtahen <macklemorellc@naver.com>
2020-07-31 11:06:03 +0900

    nothingchanged

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,11 +1,8 @@
-import React, {useState, useEffect} from "react"
+import React, {useState} from "react"
 import styles from './sidebar.module.css'
 
 export default function Sidebar ({sideList, setSideCurrent}){
   const [shown, setShown] = useState(0);
-  useEffect(()=>{
-    setSideCurrent(0);
-  },[]);
   const onClickEvent = (pressed)=>()=>{
     setSideCurrent(pressed);
     setShown(pressed);
@@ -13,7 +10,7 @@ export default function Sidebar ({sideList, setSideCurrent}){
   return(
     <div className={styles.sidebarWrapper}>
       <ul>
-        {sideList.map((item, index)=><li key={index} ><a className={styles[`sideMenu${(shown === index) ? `On` : `Off`}`]} onClick={onClickEvent(index)}>{item}</a></li>)}
+        {sideList.map((item, index)=><li key={index} ><button className={styles[`sideMenu${(shown === index) ? `On` : `Off`}`]} onClick={onClickEvent(index)}>{item}</button></li>)}
       </ul>
     </div>
   )
